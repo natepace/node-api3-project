04dd2961073ce8183abda823b7553f711925a5c9
natepace <n8wpace@gmail.com>
2021-04-08 21:39:02 -0600

    heroku stuff for deploy

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // require your server and launch it
+const express = require("express")
+const dotenv = require("dotenv").config()
 const server = require("./api/server.js")
+const port = process.env.PORT || 9000
+server.use(express.json())
 
-server.listen(999, () => {
-    console.log("server is listening to JuiceWRLD")
+server.use("/api?", (_, res) => {
+    res.json({ data: "heylook its an api!" })
+})
+server.listen(port, () => {
+    console.log(`server is listening to ${port}`)
 })
