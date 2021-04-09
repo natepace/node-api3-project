1c6347c5d489858232ec8cd89e403681cf906240
natepace <n8wpace@gmail.com>
2021-04-08 22:05:49 -0600

    heroku stuff

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState, useEffect } from 'react'
+
+function url(path) {
+  return process.env.NODE_ENV === "development"
+    ? `http://localhost:1234${path}` : path
+}
+function App() {
+
+  const [data, setData] = useState('Hi')
+  useEffect(() => {
+    fetch(url('/api/'))
+      .then(res => res.json())
+      .then(apiData => setData(apiData.data))
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        react apps are dopppee{data}
+      </header>
+    </div>
+  );
+}
+
+export default App;
